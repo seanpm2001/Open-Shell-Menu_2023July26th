dc1b122e6688d69099d42d217f2f64babacd024a
coddec <jimdoxin@gmail.com>
2018-11-06 16:34:53 +1100

    clear

--- Src/StartMenu/Legacy.h
+++
@@ -1,2 +0,0 @@
-// import legacy Classic Shell settings/data if we don't have any yet
-void ImportLegacyData();
--- clean.cmd
+++
@@ -1,5 +0,0 @@
-REM :: This script will forcefully clean the repository of residual cruft from the build process in the offchance I have forgotten
-REM :: to include something. It is not perfect but it appears to do the job.
-REM :: This is an alternative to 'git reset --hard HEAD' which would destroy local changes.
-REM :: - Xenhat
-git clean -xdf
